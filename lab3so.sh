409438dbc159090fbb23e754e57f237b51b2c094
StefanButnaru04 <161626677+StefanButnaru04@users.noreply.github.com>
2025-03-14 19:53:54 +0200

    Update lab3so.sh

--- lab3so.sh
+++ lab3so.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-head -n 15 /etc/passwd | grep '/sbin/nologin' | grep -E ':.*[m-z]$' | cut -d: -f1 > max15.txt # Problema 1
+head -n 15 /etc/passwd | grep '/sbin/nologin' | cut -d: -f1 | grep -E '[m-z]$' > max15.txt # Problema 1
 
 ps aux | grep 'bash' | grep -E 'pts/[0125]' | sort -k2,2n -r > output.txt # Problema 2
 
 ps -eo user,comm,pid --no-headers | tr -s " " | cut -d" " -f1,2,3 --output-delimiter=: | sort -k2 -t: # Problema 4
-
-
 
